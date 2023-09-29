a20ba6723f47eeff3e4a3a6cb70a15db5f177a9a
vaquero <froilan@virtual-artifact.com>
2023-09-29 15:34:50 +0100

    create command line input helper

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod apis;
 mod helpers;
 mod models;
 
+use helpers::command_line::get_user_response;
+
+
 fn main() {
-    println!("Hello, world!");
+    let user_req: String = get_user_response("What webserver are we building today?");
+
+    dbg!(user_req);
 }
